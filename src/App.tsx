34e1f0d5b535be002e1f6ce314ebf606290c4725
Alazar Gebremedhin <alazar4018@gmail.com>
2024-05-20 02:31:57 +0300

    Remove cruft.

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import type React from "react";
-
-import { Button } from "antd";
-
-export const App: React.FC = () => (
-	<div className="App">
-		<Button type="primary">Button</Button>
-	</div>
-);
